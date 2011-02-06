da7c55d34f0aae81f3775ab2d49d6993c0807531
Tim Green <timothy.green@gmail.com>
2011-02-06 15:06:59 +0000

    Fixing slug urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ import views
 urlpatterns = patterns('',
     url(r'^$', views.home, name="home"),
     url(r'^narratives/id/(?P<id>\d)$', views.narrative, name="narrative"),
-    url(r'^narratives/(?P<slug>\w)$', views.narrative, name="narrative"),
+    url(r'^narratives/(?P<slug>[\w-]+)$', views.narrative, name="narrative"),
 )
 
